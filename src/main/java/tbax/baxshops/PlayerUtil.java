feefa3c2322c4e29f1690b267e47583c91c42026
Tim Baxendale <pcluddite@outlook.com>
2019-02-17 11:01:27 -0600

    Suppress WeakerAccess warning

--- src/main/java/tbax/baxshops/PlayerUtil.java
+++ src/main/java/tbax/baxshops/PlayerUtil.java
@@ -24,6 +24,7 @@ import java.util.UUID;
 /**
  * Methods for dealing with interactions with players
  */
+@SuppressWarnings("WeakerAccess")
 public final class PlayerUtil
 {
     private PlayerUtil()
