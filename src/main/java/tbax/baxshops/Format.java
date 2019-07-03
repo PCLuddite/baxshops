e3b494bd0fa0052f6bf580e028b925c8c20bd76d
Tim Baxendale <pcluddite@outlook.com>
2019-07-03 16:38:26 -0500

    Fixed toNumeral() ArrayIndexOutOfBounds exception

--- src/main/java/tbax/baxshops/Format.java
+++ src/main/java/tbax/baxshops/Format.java
@@ -198,7 +198,7 @@ public final class Format
     }
 
     private static final String[] NUMERALS = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
-    private static final int[] NUMBERS = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 1 };
+    private static final int[] NUMBERS = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
     
     /**
      * Converts a number to a Roman numeral
@@ -214,9 +214,9 @@ public final class Format
             for (int i = 0; i < x; ++i) {
                 sb.append(NUMERALS[numeralIdx]);
             }
-            n -= x;
+            n -= x * NUMBERS[numeralIdx];
         }
-        while (++numeralIdx < NUMERALS.length);
+        while (++numeralIdx < NUMERALS.length && n > 0);
         return sb.toString();
     }
     
