7519917a5544a52f6352893e1ffb5698ac86c885
Tim Baxendale <pcluddite@outlook.com>
2019-09-27 16:03:34 -0500

    Added build parameter

--- build.sh
+++ build.sh
@@ -9,21 +9,6 @@ TMP="baxshops$RANDOM"
 echo "JAVA_HOME=$JAVA_HOME"
 echo "TMP=$TMP"
 
-compile() {
-    local VERSION=$1
-    
-    mvn install clean
-    STATUS=$?
-    if [[ $STATUS = 0 ]]; then
-        if mvn package; then
-            if [[ ! -d './bin' ]]; then
-                mkdir './bin'
-            fi
-            cp -v './target/baxshops-3.0-SNAPSHOT.jar' "./bin/baxshops-3.0-bukkit$VERSION.jar"
-        fi
-    fi
-}
-
 stash() {
     local VERSION=$1
     if [[ ! -d "/tmp/$TMP/$VERSION" ]]; then
@@ -39,15 +24,43 @@ unstash() {
     cp -vr /tmp/$TMP/$VERSION/* .
 }
 
-for DIR in versions/*; do
-    VERSION=${DIR#versions/}
-    echo "Building version $VERSION"
+compile() {
+    local VERSION=$1
+    
+    echo "Building version $VERSION..."
+    
     stash $VERSION
-    cp -vr $DIR/* .
-    compile $VERSION
+    
+    cp -vr versions/$VERSION/* .
+    mvn install clean
+    STATUS=$?
+    if [[ $STATUS = 0 ]]; then
+        if mvn package; then
+            if [[ ! -d './bin' ]]; then
+                mkdir './bin'
+            fi
+            cp -v './target/baxshops-3.0-SNAPSHOT.jar' "./bin/baxshops-3.0-bukkit$VERSION.jar"
+        fi
+    fi
+    
     unstash $VERSION
-    echo
-    echo
-done
+    
+    echo "Done."
+}
+
+if [[ "$1" != "" ]]; then
+    VERSION="$1"
+    if [[ ! -d "versions/$VERSION" ]]; then
+        echo "No version $VERSION to build"
+        exit 1
+    fi
+    compile $VERSION
+else
+    for DIR in versions/*; do
+        VERSION=${DIR#versions/}
+        compile $VERSION
+        echo
+    done
+fi
 
 rm -r /tmp/$TMP
