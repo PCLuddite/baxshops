b2c856ac80278fde02b6bfaee3edffc9e9ce424c
Tim Baxendale <pcluddite@outlook.com>
2019-09-27 15:54:13 -0500

    Added stash() to build.sh added stash() in order to save any files temporarily before they are overwritten

--- build.sh
+++ build.sh
@@ -5,12 +5,12 @@ if [ "$JAVA_HOME" = "" ]; then
     export JAVA_HOME=${JAVA_HOME%/*}
     export JAVA_HOME=${JAVA_HOME%/*}
 fi
+TMP="baxshops$RANDOM"
 echo "JAVA_HOME=$JAVA_HOME"
+echo "TMP=$TMP"
 
-for DIR in versions/*; do
-    VERSION=${DIR#versions/}
-    echo "Building version $VERSION"
-    cp -vr $DIR/* .
+compile() {
+    local VERSION=$1
     
     mvn install clean
     STATUS=$?
@@ -22,6 +22,32 @@ for DIR in versions/*; do
             cp -v './target/baxshops-3.0-SNAPSHOT.jar' "./bin/baxshops-3.0-bukkit$VERSION.jar"
         fi
     fi
+}
+
+stash() {
+    local VERSION=$1
+    if [[ ! -d "/tmp/$TMP/$VERSION" ]]; then
+        mkdir -p "/tmp/$TMP/$VERSION"
+    fi
+    for FILE in $(find "versions/$VERSION/." -type f); do
+        cp --parents -v "${FILE#versions/$VERSION/}" "/tmp/$TMP/$VERSION/"
+    done
+}
+
+unstash() {
+    local VERSION=$1
+    cp -vr /tmp/$TMP/$VERSION/* .
+}
+
+for DIR in versions/*; do
+    VERSION=${DIR#versions/}
+    echo "Building version $VERSION"
+    stash $VERSION
+    cp -vr $DIR/* .
+    compile $VERSION
+    unstash $VERSION
     echo
     echo
 done
+
+rm -r /tmp/$TMP
