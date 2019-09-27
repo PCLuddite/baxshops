cf6489c902da0ed484fedb2e242b13855ad7bb80
Tim Baxendale <pcluddite@outlook.com>
2019-09-27 14:55:20 -0500

    Build all versions

--- build.sh
+++ build.sh
@@ -6,6 +6,22 @@ if [ "$JAVA_HOME" = "" ]; then
     export JAVA_HOME=${JAVA_HOME%/*}
 fi
 echo "JAVA_HOME=$JAVA_HOME"
-if mvn install clean; then
-    mvn package
-fi
+
+for DIR in versions/*; do
+    VERSION=${DIR#versions/}
+    echo "Building version $VERSION"
+    cp -vr $DIR/* .
+    
+    mvn install clean
+    STATUS=$?
+    if [[ $STATUS = 0 ]]; then
+        if mvn package; then
+            if [[ ! -d './bin' ]]; then
+                mkdir './bin'
+            fi
+            cp -v './target/baxshops-3.0-SNAPSHOT.jar' "./bin/baxshops-3.0-bukkit$VERSION.jar"
+        fi
+    fi
+    echo
+    echo
+done
