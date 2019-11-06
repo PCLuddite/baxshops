3f1c5ced1e1c85b683e12d4ac8a6d1c2e8e1ee11
Tim Baxendale <pcluddite@outlook.com>
2019-11-05 18:45:17 -0600

    Added GeneralNotification Really just for backward compatibility

---
+++ src/main/java/tbax/shops/notification/GeneralNotification.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2013-2019 Timothy Baxendale
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+package tbax.shops.notification;
+
+import org.jetbrains.annotations.NotNull;
+import org.tbax.baxshops.serialization.StateLoader;
+
+public class GeneralNotification implements Notification
+{
+    public String message;
+
+    public GeneralNotification(final String msg) {
+        this.message = msg;
+    }
+
+    @Override
+    public org.tbax.baxshops.notification.@NotNull Notification getNewNote(StateLoader stateLoader)
+    {
+        return new org.tbax.baxshops.notification.GeneralNotification(message);
+    }
+
+    @Override
+    public @NotNull Class<? extends org.tbax.baxshops.notification.Notification> getNewNoteClass()
+    {
+        return org.tbax.baxshops.notification.GeneralNotification.class;
+    }
+}
