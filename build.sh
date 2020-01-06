a277c97f2dc6ae42987c7d22e079573a3cf82ea4
Tim Baxendale <pcluddite@outlook.com>
2020-01-06 13:35:24 -0600

    Fix build.sh

--- build.sh
+++ build.sh
@@ -1,22 +1,22 @@
+#!/bin/bash
 #
 #  Copyright (C) Timothy Baxendale
-# 
+#
 #  This library is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU Lesser General Public
 #  License as published by the Free Software Foundation; either
 #  version 2.1 of the License, or (at your option) any later version.
-# 
+#
 #  This library is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 #  Lesser General Public License for more details.
-# 
+#
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
 #  USA
 #
-#!/bin/bash
 
 if [ "$JAVA_HOME" = "" ]; then
     export JAVA_HOME=$(readlink /etc/alternatives/javac)
@@ -29,9 +29,9 @@ echo "JAVA_HOME=$JAVA_HOME"
 echo "SHOPS_VER=$SHOPS_VER"
 
 
-compile() {    
+compile() {
     echo "Building version $VERSION..."
-    
+
     mvn install clean
     STATUS=$?
     if [[ $STATUS = 0 ]]; then
@@ -42,9 +42,9 @@ compile() {
             cp -v "./target/baxshops-${SHOPS_VER}-bukkit${VERSION}-RELEASE.jar" "./bin/baxshops-${SHOPS_VER}-bukkit${VERSION}-RELEASE.jar"
         fi
     fi
-    
+
     echo "Done."
-    return $SATUS
+    return $STATUS
 }
 
 compile
